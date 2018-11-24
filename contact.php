99ede2c64dc880cf24af62f0d9220587473ee6d4
Marcio Silvestroni <marcioscp@gmail.com>
2018-11-24 19:53:01 -0200

    Initial

---
+++ contact.php
@@ -0,0 +1,60 @@
+
+<?php 
+  if(isset($_POST['email'])){
+	  
+	$name =$_POST["name"];
+	$from ="helensantos.beauty@gmail.com";
+  $fromadress =$_POST["email"];
+	$comment=$_POST["comment"];
+	// Email Receiver Address
+	$receiver="helensantos.beauty@gmail.com";
+	$subject="Contato Site";
+
+	$message = "
+	<html>
+	<head>
+	<title>HTML email</title>
+	</head>
+	<body>
+	<table width='50%' border='0' align='center' cellpadding='0' cellspacing='0'>
+	<tr>
+	<td colspan='2' align='center' valign='top'><img style=' margin-top: 15px; ' src='http://www.helensantos.com.br/images/logo-helen-dark.png' width='135'></td>
+	</tr>
+	<tr>
+	<td width='50%' align='right'>&nbsp;</td>
+	<td align='left'>&nbsp;</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'>Nome:</td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>".$name."</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'>E-mail:</td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>".$fromadress."</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; border-bottom:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'>Mensagem:</td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; border-bottom:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>".nl2br($comment)."</td>
+	</tr>
+	</table>
+	</body>
+	</html>
+	";
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+// More headers
+$headers .= 'From: <'.$from.'>' . "\r\n";
+   if(mail($receiver,$subject,$message,$headers))  
+   {
+	   //Success Message
+      echo "Mensagem enviada com sucesso";
+   }
+   else
+   {	
+   	 //Fail Message
+      echo "Sua mensagem não pode ser enviada :/ ";
+   }
+
+}
+?>
