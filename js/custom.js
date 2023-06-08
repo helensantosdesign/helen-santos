ca468853f0167fcfe91ab19ef262224c0fd561b4
Marcio Pinheiro <marcio.pinheiro@farfetch.com>
2023-06-08 18:00:42 +0100

    update index

--- js/custom.js
+++ js/custom.js
@@ -252,26 +252,7 @@ $(document).ready(function () {
 
 
     $(window)
-            .scroll(function () {
-                if ($(this)
-                        .scrollTop() > 100) {
-                    $('.scrollToTop')
-                            .fadeIn();
-                } else {
-                    $('.scrollToTop')
-                            .fadeOut();
-                }
-            });
-
-    //Click event to scroll to top
-    $('.scrollToTop')
-            .click(function () {
-                $('html, body')
-                        .animate({
-                            scrollTop: 0
-                        }, 1000);
-                return false;
-            });
+            .scroll();
 
     /* =================================
      easyPieChart
